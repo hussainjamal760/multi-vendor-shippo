508a2a8397bea50ea2a7a096cd542ed7f085984e
Hussain Jamal <Hjamal9865@gmail.com>
2025-10-07 01:53:51 +0500

    sendMail updated

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -4,8 +4,7 @@ const sendMail = async (options) => {
     const transporter = nodemailer.createTransport({
         host: process.env.SMPT_HOST,
         port: process.env.SMPT_PORT,
-        service: process.env.SMPT_SERVICE,
-        auth:{
+        auth: {
             user: process.env.SMPT_MAIL,
             pass: process.env.SMPT_PASSWORD,
         },
