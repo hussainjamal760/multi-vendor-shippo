a3eb5315743083495e247e51a6e948575dc94454
Hussain Jamal <Hjamal9865@gmail.com>
2025-10-12 17:19:31 +0500

    size changed

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -222,7 +222,7 @@ const Header = ({ activeHeading }) => {
               <img
                 src="main.png"
                 alt=""
-                className="h-10 w-auto cursor-pointer" 
+                className="h-30 w-auto cursor-pointer" 
               />
             </Link>
           </div>
